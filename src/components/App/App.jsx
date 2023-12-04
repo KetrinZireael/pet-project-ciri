3ed10f71dd5b15f00d7ed0651db9b18bbfb87577
Kateryna-WLA <geleczkak26@gmail.com>
2023-12-04 18:04:35 +0200

    add material ui

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,24 +2,30 @@ import { useState } from "react";
 import projectLogo from "../../assets/logo_site.png";
 import "./App.css";
 
+import CssBaseline from '@mui/material/CssBaseline';
+
+import ResponsiveAppBar from '../Header/Header';
+
 import Title from "../Title/Title";
 
 function App() {
   return (
     <>
-      <div>
+      <CssBaseline>
+        <ResponsiveAppBar/>
         <a
           href="https://github.com/KetrinZireael/pet-project-ciri"
           target="_blank"
         >
-          <img
+          {/* <img
             src={projectLogo}
             className="logo react"
             alt="Pet Project Ciri"
-          />
+          /> */}
         </a>
-      </div>
-      <Title heading = "Pet Project Ciri"/>
+              {/* <Title heading = "Pet Project Ciri"/> */}
+
+      </CssBaseline>
     </>
   );
 }
